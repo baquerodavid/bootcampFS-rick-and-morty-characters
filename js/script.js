7e0c2f8d9a4dcb6d9f0f307c4bf5ebf7699381e6
David Baquero <davidbaquerogonzalez@gmail.com>
2025-11-12 21:46:32 +0100

    Actualizado con el código de la Live Review

--- js/script.js
+++ js/script.js
@@ -111,3 +111,80 @@ prevPageBtn.addEventListener('click', () => {
       });
   }
 });
+
+
+
+// 👇 CODIGO DE LA LIVE REVIEW EMPIEZA DESDE AQUÍ 👇
+
+/*   ---------   INICIO COMENTARIO   -----------
+
+const prevPage = document.getElementById('prev-page');
+const nextPage = document.getElementById('next-page');
+const characterList = document.getElementById("character-list")
+let currentPage = 1;
+
+// Con el map, solo renderiza el DOM una vez, porque guarda los datos en un array, y luego los suelta todos los datos de una vez. Entonces a nivel performance es mucho mejor
+
+function getCharacters() {
+  return fetch(`https://rickandmortyapi.com/api/character/?page=${currentPage}`) //Pedir los datos de el end point
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error en la petición: ${response.status}')
+      }
+      return response.json(); //los datos que vienen los parseamos a un formato legible para JS (JSON)
+    })
+    .then(data => {
+      const characters = data.results.map(character => {
+        const template = `
+         <li>
+          <img src="${character.image}" alt="${character.name}" />
+          <h2>Name: ${character.name}</h2>
+          <p>Species: ${character.species}</p>
+        </li >
+      `
+        return template
+      }).join("")
+      characterList.innerHTML = characters;
+      removeButton();
+    })// Obtenemos los datos que están en el endpoint ya parseados y preparados para usar
+    .catch(err => console.log(err));
+}
+
+function removeButton() {
+  currentPage === 1   ? prevPage.classList.add("disabled") : prevPage.classList.remove("disabled");
+  currentPage === 42  ? nextPage.classList.add("disabled") : nextPage.classList.remove("disabled");
+}
+
+prevPage.addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    //console.log(currentPage);
+    getCharacters();
+  }
+})
+
+
+nextPage.addEventListener("click", () => {
+  if (currentPage < 42) {
+    currentPage++;
+    getCharacters();
+  }
+})
+--------------- FIN COMENTARIO ---------    */
+
+/*
+//Con el forEach está renderizando en cada vuelta en nuestro DOM, por lo que a nivel performance es peor
+fetch("https://rickandmortyapi.com/api/character/?page=1")
+  .then(response => response.json())
+  .then(data => {
+    data.results.forEach(character => {
+      const template = `
+         <li>
+          <img src="${character.image}" alt="${character.name}" />
+          <h2>${character.name}</h2>
+        </li >
+      `
+      characterList.innerHTML += template;
+    });
+  })// Obtenemos los datos que están en el endpoint ya parseados y preparados para usar
+*/
